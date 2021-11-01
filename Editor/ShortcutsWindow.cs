9f10edeb2059bdc99fc2df0ac272083c8dfbe903
Poroh-kun <poroh.kun@gmail.com>
2021-11-01 13:09:45 +0300

    editor assembly create

--- Editor/ShortcutsWindow.cs
+++ Editor/ShortcutsWindow.cs
@@ -1,6 +1,4 @@
-﻿#if UNITY_EDITOR
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEditor;
@@ -235,4 +233,3 @@ public class ShortcutsWindow : EditorWindow
     }
     #endregion
 }
-#endif
